d78a46b2230f238833d7a288545c867fbb553fb6
Cortina17 <diego.cortinagarcia@gmail.com>
2022-02-16 15:26:59 +0100

    footer styles

--- atomicfilms/src/App.js
+++ atomicfilms/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import FooterPage from "./components/Footer";
 import { MovieList } from "./components/MovieList";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <MovieList />
       <FooterPage />
     </div>
--- atomicfilms/src/components/Footer.jsx
+++ atomicfilms/src/components/Footer.jsx
@@ -1,8 +1,11 @@
 const FooterPage = () => {
     return (
-        <div style={{ height: '10%', color: 'blue', backgroundColor: 'lightblue' }}>
-            <h4>Equipo 2</h4>
-            <p>Albert, Ana, Christian, Diego, Jymmy, Martin, Vicky</p>
+        // <div style={{ height: '10%', color: 'blue', backgroundColor: 'lightblue' }}>
+        <div class="d-flex justify-content-center pt-5 mt-2">
+            <div class="bg-dark color:whithe w-100 " style={{color: 'white'}}>
+                <h4 class="d-flex justify-content-center">Equipo 2</h4>
+                <p class="d-flex justify-content-center">Albert, Ana, Christian, Diego, Jymmy, Martin, Vicky</p>
+            </div>
         </div>
     )
 }
