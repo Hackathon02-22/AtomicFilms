988fbe858a4ffdcced85d818b3a5c7b386b17ec2
Cortina17 <diego.cortinagarcia@gmail.com>
2022-02-16 14:01:43 +0100

    footer component

--- atomicfilms/src/App.js
+++ atomicfilms/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import FooterPage from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      
+      <FooterPage />
     </div>
   );
 }
---
+++ atomicfilms/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+const FooterPage = () => {
+    return (
+        <div style={{ height: '10%', color: 'blue', backgroundColor: 'lightblue' }}>
+            <h4>Equipo 2</h4>
+            <p>Albert, Ana, Christian, Diego, Jymmy, Martin, Vicky</p>
+        </div>
+    )
+}
+
+export default FooterPage;
