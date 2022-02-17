8f6d54f0601863c665485ef88139dacffa9c0627
martincholt <martindomingo25@gmail.com>
2022-02-17 16:15:15 +0100

    change language api to english

--- atomicfilms/src/service/movieService.js
+++ atomicfilms/src/service/movieService.js
@@ -1,4 +1,4 @@
 export async function getAllMovies() {
-    const res = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=f0804310b2aa799c42575717c6a0eae1&language=es-MX', { method: "GET" });
+    const res = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=f0804310b2aa799c42575717c6a0eae1&language=en', { method: "GET" });
     return await res.json();
 }
