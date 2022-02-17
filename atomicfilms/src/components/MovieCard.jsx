62816dffc6cc2b0bfe9da6e7a3381d2e7471e861
Jymmy Murillo <murillojymmy@gmail.com>
2022-02-17 03:47:36 -0500

    adding movie poster

--- atomicfilms/src/components/MovieCard.jsx
+++ atomicfilms/src/components/MovieCard.jsx
@@ -1,14 +1,18 @@
 
 
-export function MovieCard(props){
+export function MovieCard(props) {
     return (
-        <div>
-            <div className="card">
-                <div className="movie-name">
-                    Movie: {props.movie.original_title}
-                </div>
-            </div>
-        </div>
+      <div>
+        <div className="card">
+          <div className="poster-card">
+            <img
+              src={`https://image.tmdb.org/t/p/w500/${props.movie.poster_path}`}
+              alt="Moivie Poster"
+            />
+          </div>
 
-    )
+          <div className="movie-name">Movie: {props.movie.title}</div>
+        </div>
+      </div>
+    );
 }
