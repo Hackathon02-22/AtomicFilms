6910114944e7f73f134c7576df5693885fae8908
Jymmy Murillo <murillojymmy@gmail.com>
2022-02-17 11:25:58 -0500

    resolving conflicts when pulling from Jymmy

--- atomicfilms/src/components/Footer.jsx
+++ atomicfilms/src/components/Footer.jsx
@@ -1,10 +1,10 @@
 const FooterPage = () => {
     return (
         // <div style={{ height: '10%', color: 'blue', backgroundColor: 'lightblue' }}>
-        <div class="d-flex justify-content-center pt-5 mt-2 fixed-bottom">
+        <div class="d-flex justify-content-center">
             <div class="bg-dark color:whithe w-100 " style={{color: 'white'}}>
-                <h4 class="d-flex justify-content-center">Equipo 2</h4>
-                <p class="d-flex justify-content-center">Albert, Ana, Christian, Diego, Jymmy, Martin, Vicky</p>
+                <h4 class="d-flex justify-content-center pt-4"><strong>Equipo 2</strong> </h4>
+                <p class="d-flex justify-content-center">Albert - Ana - Christian - Diego -Jymmy - Martin - Vicky</p>
             </div>
         </div>
     )
