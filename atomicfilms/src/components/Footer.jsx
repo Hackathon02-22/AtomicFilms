1b5ec45f781f427b08586a21048c991aab8945d6
Cortina17 <diego.cortinagarcia@gmail.com>
2022-02-17 17:01:25 +0100

    readme and footer modified

--- atomicfilms/src/components/Footer.jsx
+++ atomicfilms/src/components/Footer.jsx
@@ -1,10 +1,11 @@
 const FooterPage = () => {
     return (
         // <div style={{ height: '10%', color: 'blue', backgroundColor: 'lightblue' }}>
-        <div class="d-flex justify-content-center">
-            <div class="bg-dark color:whithe w-100 " style={{color: 'white'}}>
-                <h4 class="d-flex justify-content-center pt-4"><strong>Equipo 2</strong> </h4>
-                <p class="d-flex justify-content-center">Albert - Ana - Christian - Diego -Jymmy - Martin - Vicky</p>
+        <div className="d-flex justify-content-center">
+            <div className="bg-dark color:whithe w-100 " style={{color: 'white'}}>
+                <h4 className="d-flex justify-content-center pt-4"><strong>Equipo 2</strong> </h4>
+                <h5 className="d-flex justify-content-center">Albert - Ana - Christian - Diego -Jymmy - Martin - Vicky</h5>
+                <p className="d-flex justify-content-center">Hackaton F5 2022, Adevinta. ©</p>
             </div>
         </div>
     )
