e11144ffa06c090442b92611cf73774601416035
Cortina17 <diego.cortinagarcia@gmail.com>
2022-02-17 10:27:42 +0100

    minor styles

--- atomicfilms/src/components/MovieCard.jsx
+++ atomicfilms/src/components/MovieCard.jsx
@@ -1,7 +1,8 @@
 export function MovieCard(props) {
-    return (
-      <div>
-        <div className="card">
+  return (
+    <div className="movie-body row align-items-center">
+      <div class="row align-items-center">
+        <div className="card mt-5 mb-5 pt-5 pb-5 w-50">
           <div className="poster-card">
             <img
               src={`https://image.tmdb.org/t/p/w500/${props.movie.poster_path}`}
@@ -11,5 +12,6 @@ export function MovieCard(props) {
           <div className="movie-name">Movie: {props.movie.title}</div>
         </div>
       </div>
-    );
+      </div>
+      );
 }
